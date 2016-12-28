73e4de1d975e5b6e5d3b0c7c92fe9e66b833d4ac
Joshua Jones <fishnibble1@gmail.com>
2016-12-28 10:32:03 -0500

    Update
    Fixed the issue with it only having 1 folder made when the makefolders method is called

--- vocabsort.py
+++ vocabsort.py
@@ -3,20 +3,24 @@ import urllib.request
 from bs4 import BeautifulSoup
 import os, os.path
 
+
 class naverscrape(object):
     def __init__(self, urlpayload, path):
         self.urlpayload = urlpayload
         self.path = path
-        url = requests.get(urlpayload)
-        self.soup = BeautifulSoup(url, "html.parser")
 
     def payloadprep(self):
-        url = []
-        for text in self.soup.find_all_next('div', class_='levels clearfix'):
+        urlarray = []
+        url = requests.get(self.urlpayload).content
+        soup = BeautifulSoup(url, "html.parser")
+
+        for text in soup.find_all('div', class_='levels clearfix'):
             for links in text.find_all('a'):
-                url.append("http://www.memrise.com" + links.get('href'))
-        return url
+                urlarray.append("http://www.memrise.com" + links.get('href'))
+        return urlarray
+
     # Goes to main list of chapters and grabs all the urls for each chapter
+
 
     def getvocab(self):
         url = self.payloadprep()
@@ -34,25 +38,28 @@ class naverscrape(object):
                 test += vocab.text + '\n'
                 if '갑자기' in test:
                     souparray.append([test])
-                #need to fix this loop so it can be used for any course
+                    # need to fix this loop so it can be used for any course
         return souparray
-        #Goes to everything in the payload and grabs all the vocab in every URL
-        #col_a col text is every all the korean text is at
+        # Goes to everything in the payload and grabs all the vocab in every URL
+        # col_a col text is every all the korean text is at
 
     def makefolders(self):
-        counter = 0
-        vocab = self.getvocab()
-        if not os.path.isdir(self.path + "{0}".format(counter)):
-            os.mkdir(self.path + "{}".format(counter))
+        counter = 1
+        urls = self.payloadprep()
+        for links in urls:
+            if not os.path.isdir(self.path + "{0}".format(counter)):
+                os.mkdir(self.path + "{}".format(counter))
+                counter += 1
 
     def getmp3(self):
         for root, dirnames, filenames in os.walk(self.path):
             for files in filenames:
                 if files.endswith('.txt'):
-                    f = open(os.path.join(root, files) ,'r', encoding='UTF-8')
+                    f = open(os.path.join(root, files), 'r', encoding='UTF-8')
                     for stuff in f:
                         vocab = stuff.replace('.', '')
-                        rawurl = requests.get('http://m.krdic.naver.com/search/all/0/{0}?format=HTML&isMobile=true'.format(vocab)).content
+                        rawurl = requests.get(
+                            'http://m.krdic.naver.com/search/all/0/{0}?format=HTML&isMobile=true'.format(vocab)).content
                         soup = BeautifulSoup(rawurl, "html.parser")
                         mp3url = []
                         for text in soup.find_all('div', class_='dt'):
@@ -63,4 +70,3 @@ class naverscrape(object):
                                 print("Downloaded {0}".format(stuff) + 'in {0}'.format(root))
                             except:
                                 pass
-
